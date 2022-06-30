aa67879145fa639d53ae16373952e25ef035ff52
Gerardo Fumero <gerardofumero14@gmail.com>
2022-06-30 11:09:33 -0500

    Se inicia el proyecto

---
+++ app/src/main/java/com/example/semestral_android_1sf142/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.semestral_android_1sf142;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
